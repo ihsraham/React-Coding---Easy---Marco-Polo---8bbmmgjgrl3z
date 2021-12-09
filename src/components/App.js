264959794136456d0a7921242b7d007333f2d573
Maharshi Mishra <ihsraham27@gmail.com>
2021-12-09 19:18:36 +0530

    Made some changes.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
+import { useState } from "react";
 const App = () => {
-  
+  const [marco, setMarco] = useState(true);
   return (
     <div id="main">
-
+      <h1 id="marco- polo">{marco ? "Marco" : "Polo"}</h1>
+      <button id="marco- polo- toggler" onClick={() => setMarco(!marco)}>
+        {marco ? "Polo" : "Marco"}
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
